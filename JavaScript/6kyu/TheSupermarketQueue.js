8f4ab4e7fff94646d990b08c2c63e2b319566525
ridvanms <ridvans@abv.bg>
2026-01-31 13:26:36 +0200

    Add queueTime function and problem description

---
+++ JavaScript/6kyu/TheSupermarketQueue.js
@@ -0,0 +1,10 @@
+function queueTime(customers, n) {
+  const tills = Array(n).fill(0);
+  
+  for (let time of customers){
+    const idx = tills.indexOf(Math.min(...tills));
+    tills[idx] += time;
+  }
+  
+  return Math.max(...tills);
+}
